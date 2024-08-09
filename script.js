db62179d8184ca00211eb0af1783a94ca188421e
GSajeendra <gayathrisajeendra@gmail.com>
2024-08-09 19:47:17 +0100

    Set up basic layout and structure for portfolio website.

---
+++ script.js
@@ -0,0 +1,9 @@
+function  showSection(sectionId) {
+    // Hide all sectoins
+    document.querySelectorAll('main.content section').forEach(function(section) {
+       section.style.display = 'none';
+    });
+
+    // Show the selected section
+    document.getElementById(sectionId).style.display = 'black';
+}
